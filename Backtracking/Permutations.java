cff192c4c9527ef3162c90b88e86e50b7c43d603
subhasish <subhasish.kundu3@gmail.com>
2025-07-26 13:53:42 +0530

    Backtracking Problem: Generating permutations.

---
+++ Backtracking/Permutations.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode: https://leetcode.com/problems/permutations/description/
+ */
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        permutationGenerator(nums, new ArrayList<>(), result);
+        return result;
+    }
+    private void permutationGenerator(int[] nums, List<Integer> currentSubset, List<List<Integer>> result) {
+        if (currentSubset.size() == nums.length) {
+            result.add(new ArrayList<>(currentSubset));
+            return;
+        }
+        for (int i=0; i<nums.length; i++) {
+            if (currentSubset.contains(nums[i])) {
+                continue;
+            }
+            currentSubset.add(nums[i]);
+            permutationGenerator(nums, currentSubset, result);
+            currentSubset.removeLast();
+        }
+    }
+}
