a3e647a640f62d29d3af0408db745d84368fbc38
subhasish <subhasish.kundu3@gmail.com>
2025-07-26 20:01:51 +0530

    Heap: Kth Largest Element in an array.

---
+++ Heap/KthLargetElementInAnArray.java
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
+ */
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for(int i=0; i<nums.length; i++) {
+            heap.offer(nums[i]);
+            if(heap.size() > k) {
+                heap.poll();
+            }
+        }
+        return heap.peek();
+    }
+}
