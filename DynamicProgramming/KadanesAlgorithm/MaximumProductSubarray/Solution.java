d59f5b09acd0d1ece8bc11b3da3276aaadf829f2
subhasish <subhasish.kundu3@gmail.com>
2025-07-29 23:24:25 +0530

    Maximum Product Subarray.

---
+++ DynamicProgramming/KadanesAlgorithm/MaximumProductSubarray/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/maximum-product-subarray/
+ */
+class Solution {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+        int result = maxSoFar;
+
+        for(int i=1; i<nums.length; i++) {
+            int curr = nums[i];
+
+            int tempMax = Math.max(curr, Math.max(maxSoFar*curr, minSoFar*curr));
+            minSoFar = Math.min(curr, Math.min(maxSoFar*curr, minSoFar*curr));
+            maxSoFar = tempMax;
+            result = Math.max(maxSoFar, result);
+        }
+        return result;
+    }
+}
