bf01fb40a41fc220cf34b71482be1ef006e3fcba
subhasish <subhasish.kundu3@gmail.com>
2025-07-30 23:27:36 +0530

    Russian Envelopes.

---
+++ DynamicProgramming/LongestIncreasingSubsequence/RussianDolls/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * https://leetcode.com/problems/russian-doll-envelopes/
+ * Regular LIS of O(n^2) throws TLE. Hence requires binary search.
+ * In this case, we find what should be place of the element in subsequence.
+ * Then we would increase length of the subsequence. 
+ */
+class Solution {
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) {
+                len++;
+            }
+        }
+        return len;
+    }
+
+    public int maxEnvelopes(int[][] envelopes) {
+        // sort on increasing in first dimension and decreasing in second
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] arr1, int[] arr2) {
+                if (arr1[0] == arr2[0]) {
+                    return arr2[1] - arr1[1];
+                } else {
+                    return arr1[0] - arr2[0];
+                }
+           }
+        });
+        // extract the second dimension and run LIS
+        int[] secondDim = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; ++i) secondDim[i] = envelopes[i][1];
+        return lengthOfLIS(secondDim);
+    }
+}
+
